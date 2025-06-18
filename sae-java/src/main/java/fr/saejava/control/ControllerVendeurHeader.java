57848498fe406a0c16501387f7b0dcf4210862aa
colin-44 <146717953+colin-44@users.noreply.github.com>
2025-06-18 16:16:12 +0200

    Add files via upload

---
+++ sae-java/src/main/java/fr/saejava/control/ControllerVendeurHeader.java
@@ -0,0 +1,34 @@
+package fr.saejava.control;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class ControllerVendeurHeader {
+
+    @FXML
+    private Button buttonGererStock;
+
+    @FXML
+    private Button buttonTransfererLivre;
+
+    @FXML
+    private Button buttonVerifierDispo;
+
+    @FXML
+    void gererStock(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tranfererLivre(MouseEvent event) {
+
+    }
+
+    @FXML
+    void verifierDispo(MouseEvent event) {
+
+    }
+
+}
---
+++ sae-java/src/main/java/fr/saejava/model/ChangementBD.java
@@ -0,0 +1,42 @@
+package fr.saejava.model;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.io.File;
+import java.io.FileWriter;
+
+/*
+ * Class qui permet de récupérer les nom et prénom des clients pour créer des username et mdp
+ */
+public class ChangementBD {
+    public static void main(String[] args) {
+        try{
+        ConnexionMySQL connexion = new ConnexionMySQL();
+        connexion.connecter("servinfo-maria", "Librairie", "maubert", "maubert");
+        Statement st = connexion.createStatement();
+        ResultSet r = st.executeQuery("SELECT * FROM CLIENT");
+        // Création du fichier
+        File myObj = new File("insertUtilisateur.txt");
+        if (myObj.createNewFile()) {
+            System.out.println("Fichier créer: " + myObj.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
+        // Ecriture dans le fichier
+        FileWriter myWriter = new FileWriter("insertUtilisateur.txt");
+        myWriter.write("insert into UTILISATEUR(idutilisateur, nom, prenom, username, motdepasse) values"+"\n");
+        while(r.next()){
+            String username = r.getString("prenomcli")+r.getInt("idcli");
+            String password = Math.abs(r.getString("adressecli").hashCode())+r.getString("nomcli");
+            myWriter.write("("+r.getInt("idcli")+",'"+r.getString("nomcli")+"','"+r.getString("prenomcli")+"','"+username+"','"+password+"'"+"),"+"\n");
+        }
+        r.close();
+        st.close();
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
+        }
+        catch(Exception e){
+            System.out.print(e.getMessage());
+        }
+    }
+}
