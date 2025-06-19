74cd3df281c9515a020f148c1a36e9bea385de5c
icosme <ilona.cosme-vinou@etu.univ-orleans.fr>
2025-06-19 09:20:02 +0200

    controleur

--- sae-java/src/main/java/fr/saejava/control/ControllerClientAccueil.java
+++ sae-java/src/main/java/fr/saejava/control/ControllerClientAccueil.java
@@ -3,14 +3,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
-import java.net.URL;
-import java.util.ResourceBundle;
 import java.sql.SQLException;
 import fr.saejava.ApplicationLibrairie;
 import fr.saejava.model.*;
-import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.scene.control.Label;
 
 
 
@@ -40,9 +36,7 @@ public class ControllerClientAccueil{
     @FXML
     public void initialize() {
         try{
-            listViewMeiileurVentes.setItems(FXCollections.observableArrayList(clientBD.getMeilleurVente()));
-
-        }
+            listViewMeiileurVentes.setItems(FXCollections.observableArrayList(clientBD.getMeilleurVente()));}
         catch(SQLException e){
             app.afficherErreur("Erreur lors de la récupération des meilleurs vente");
         }
